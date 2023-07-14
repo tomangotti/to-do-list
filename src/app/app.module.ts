77e0aad41b8a07afd5d119637d4fca11dfb2b1e8
thomas <tom.angotti11@gmail.com>
2023-07-13 20:47:36 -0600

    create header and generated top teir components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FormComponent } from './form/form.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FormComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
